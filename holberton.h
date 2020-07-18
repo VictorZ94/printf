ee4c62c854b2459e16c3e3aafde499e69847afa5
Valentinaga1 <Valentinaga1>
2020-07-18 19:19:29 +0000

    Holberton Header update with functions

--- holberton.h
+++ holberton.h
@@ -1,7 +1,17 @@
 #ifndef PRINTF_H
 #define PRINTF_H
 
+#include <stdarg.h>
+
+typedef struct print
+{
+	char c;
+	void (*f)(va_list);
+}print;
+
 int _printf(const char *format, ...);
 int _putchar(char c);
+void printchar(va_list args);
+void printstring(va_list args);
 
-#endif PRINTF_H
+#endif 
