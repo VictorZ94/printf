c0d58b5206f33ebae84410b69199470d81637d99
Valentinaga1 <Valentinaga1>
2020-07-18 22:30:50 +0000

    Adding functions for integers

--- functions.c
+++ functions.c
@@ -19,3 +19,39 @@ void printstring(va_list args)
 		s++;
 	}
 }
+
+void printint(va_list args)
+{
+	char lastDigit;
+	int reversed;
+	int n;
+
+	n = va_arg(args, int);
+
+	if (n < 0)
+	{
+	_putchar('-');
+	lastDigit = ('0' - (n % 10));
+	n /= -10;
+	}
+	else
+	{
+	lastDigit = ((n % 10) + '0');
+	n /= 10;
+	}
+
+	reversed = 0;
+	while (n > 0)
+	{
+	reversed = reversed * 10 + (n % 10);
+	n /= 10;
+	}
+
+	while (reversed > 0)
+	{
+	char c = ((reversed % 10) + '0');
+	_putchar(c);
+	reversed /= 10;
+	}
+	_putchar(lastDigit);
+}
