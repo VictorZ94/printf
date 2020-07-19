c6ed3cd7ff34d623a0c45c501d3fb057113b82ae
Victor Zuluaga <zrvictor@outlook.com>
2020-07-19 23:16:20 +0000

    first version printf

---
+++ _printf.c
@@ -0,0 +1,43 @@
+#include <stdarg.h>
+#include "holberton.h"
+#include <stdio.h>
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0;
+	int j;
+	int count = 0;
+	print form[] = {
+		{"%", printporcent},
+		{"c", printchar},
+		{"s", printstring},
+		{NULL, NULL}
+	};
+
+	if (!format)
+		return (-1);
+
+	if (format[i] == '%' && !format[i + 1])
+		return (-1);
+
+	va_start(args, format);
+	i = 0;
+	while (format[i] != '\0')
+	{
+		j = 0;
+		while (form[j].c != '\0' && format[i] == '%')
+		{
+			if (format[i + 1] == form[j].c[0])
+			{
+				count = form[j].f(args);
+				i += 2;
+			}
+			j++;
+		}
+		_putchar(format[i]);
+		i++;
+	}
+	va_end(args);
+	return (i + count);
+}
