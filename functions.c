1a84d2df7c0bf36e9f084b0a5727b12e30e2c351
Victor Zuluaga <zrvictor@outlook.com>
2020-07-19 23:50:55 +0000

    functions files in this file we have all functions for print format

--- functions.c
+++ functions.c
@@ -2,18 +2,32 @@
 #include "holberton.h"
 #include <stdlib.h>
 
+/**
+ * printchar - function to print char data types
+ * @args: - argument variadic function
+ *
+ * Return: -1
+ */
 int printchar(va_list args)
 {
 	char c;
+
 	c = va_arg(args, int);
-	_putchar(c);
-	return(-1);
+	_putchar (c);
+	return (-1);
 }
 
+/**
+ * printstring - function to print string data types
+ * @args: - argument variadic function
+ *
+ * Return: lenght string
+ */
 int printstring(va_list args)
 {
 	char *s;
 	int i;
+
 	s = va_arg(args, char *);
 	if (s == NULL)
 		return (-1);
@@ -23,13 +37,18 @@ int printstring(va_list args)
 		_putchar(s[i]);
 		i++;
 	}
-	return(i - 2);
+	return (i - 2);
 }
 
+/**
+ * printporcent - function to print percentage symbol data types
+ * @args: - argument variadic function
+ *
+ * Return: -1
+ */
 int printporcent(va_list args)
 {
 	(void)args;
 	_putchar('%');
-	return(-1);
+	return (-1);
 }
-
