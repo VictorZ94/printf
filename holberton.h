7508b1e23f8fad87f2ca54abef70353977df5567
Victor Zuluaga <zrvictor@outlook.com>
2020-07-21 01:36:08 +0000

    added prototypes space

--- holberton.h
+++ holberton.h
@@ -2,17 +2,12 @@
 #define PRINTF_H
 /*Holberton header*/
 #include <stdarg.h>
-/**
- * struct print - Structure of string of characters and functions of _printf.
- * @c: format for print
- * @f: Functions to the format
- */
 
 typedef struct print
 {
 	char *c;
 	int (*f)(va_list);
-} print;
+}print;
 
 int _printf(const char *format, ...);
 int _putchar(char c);
@@ -20,5 +15,6 @@ int printchar(va_list args);
 int printstring(va_list args);
 int printporcent(va_list args);
 int printint(va_list args);
+int printspace(va_list args);
 
-#endif
+#endif 
